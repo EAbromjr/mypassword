2157348da3226a0e217cb695ec045b1f62901be4
Earnest Abrom <156559830+EAbromjr@users.noreply.github.com>
2024-06-24 14:12:04 -0500

    Defined user class

--- user_class.py
+++ user_class.py
@@ -1,5 +1,11 @@
 class User:
-  def __init__(self, username, login_password): 
+  def __init__(self, username, login_password, email, first_name, last_name): 
     # Indentation fixed here 
     self.username = username
     self.login_password = login_password  
+    self.email = email
+    self.first_name = first_name
+    self.last_name = last_name
+
+
+  
