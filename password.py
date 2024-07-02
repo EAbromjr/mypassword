ca7929decb083484bb0efe3a9161df30e29453f0
earnestabromjr <abromearnest@gmail.com>
2024-07-02 13:02:16 -0500

    Update password.py

--- password.py
+++ password.py
@@ -1,5 +1,14 @@
 import csv
 
+class Password:
+    def __init__(self) -> None:
+       pass 
+
+    def read_csv(self, file_path: str):
+        with open(file_path, 'r') as file:
+            reader = csv.reader(file)
+            return reader
+
 # Read from Chrome Passwords.csv
 with open('Chrome Passwords.csv', 'r') as file:
     reader = csv.reader(file)
